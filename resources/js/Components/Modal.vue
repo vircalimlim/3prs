c37dc43e057661408232c89781badd08239267dd
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-08-30 14:59:35 +0800

    Update Modal.vue

--- resources/js/Components/Modal.vue
+++ resources/js/Components/Modal.vue
@@ -84,7 +84,7 @@ const maxWidthClass = computed(() => {
                 >
                     <div
                         v-show="show"
-                        class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:mx-auto"
+                        class="p-4 mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:mx-auto"
                         :class="maxWidthClass"
                     >
                         <slot v-if="show" />
