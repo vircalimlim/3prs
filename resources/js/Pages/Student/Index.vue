66109cde8f83bdc5815148f372dcfa56b53f03ea
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-09-02 16:19:17 +0800

    update footer.

--- resources/js/Pages/Student/Index.vue
+++ resources/js/Pages/Student/Index.vue
@@ -3,6 +3,7 @@ import { Head, Link } from '@inertiajs/vue3';
 import { ref, onMounted, onBeforeUnmount } from 'vue';
 import StudentNavbar from '@/Components/StudentNavbar.vue';
 import TextHeader from '@/Components/TextHeader.vue';
+import Footer from '@/Components/Footer.vue';
 
 const isHidden = ref(false);
 let lastScrollTop = 0;
@@ -259,25 +260,6 @@ header {
             </masonry-wall>
         </section>
 
-        <footer class="bg-white rounded-lg shadow mt-20">
-            <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-                <div class="sm:flex sm:items-center sm:justify-between">
-                    <a href="" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                        <span class="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
-                    </a>
-                    <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
-                        <li>
-                            <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-                <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-                <span class="block text-sm text-gray-500 sm:text-center">© 2024 3PRS™.</span>
-            </div>
-        </footer>
+        <Footer />
     </main>
 </template>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,6 +2,7 @@
 import { Head, Link } from '@inertiajs/vue3';
 import { ref, onMounted, onBeforeUnmount } from 'vue';
 import TextHeader from '@/Components/TextHeader.vue';
+import Footer from '@/Components/Footer.vue';
 
 const isHidden = ref(false);
 let lastScrollTop = 0;
@@ -56,10 +57,10 @@ header {
                 <img src="" alt="website logo">
             </div>
             <div>
-                <button type="button"
+                <a href="/login" type="button"
                     class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center me-2 mb-2">
                     Login
-                </button>
+                </a>
             </div>
         </header>
 
@@ -269,25 +270,6 @@ header {
 
 
 
-        <footer class="bg-white rounded-lg shadow mt-20">
-            <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-                <div class="sm:flex sm:items-center sm:justify-between">
-                    <a href="" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                        <span class="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
-                    </a>
-                    <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
-                        <li>
-                            <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-                <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-                <span class="block text-sm text-gray-500 sm:text-center">© 2024 3PRS™.</span>
-            </div>
-        </footer>
+        <Footer />
     </main>
 </template>
