cb1d69ad547b8b7befff777533a1f45afc4ed3f9
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-09-12 17:15:31 +0800

    admin > fixed menu on mobile screen.

--- resources/js/Layouts/Admin.vue
+++ resources/js/Layouts/Admin.vue
@@ -5,48 +5,23 @@ import { ref } from "vue";
 const isShowDropDown = ref(false);
 const isShowDropDown2 = ref(false);
 
-// onMounted(() => {
-//   document.addEventListener('contextmenu', function(e) {
-//     e.preventDefault();
-//   });
+const toggleMenu = ref(false);
 
-//   document.onkeydown = (e) => {
-//     if (e.key == 123) {
-//         e.preventDefault();
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.key == 'I') {
-//         e.preventDefault();
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.key == 'C') {
-//         e.preventDefault();
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.key == 'J') {
-//         e.preventDefault();
-//     }
-//     if (e.ctrlKey && e.key == 'U') {
-//         e.preventDefault();
-//     }
-//     if ((e.ctrlKey || e.metaKey) && e.key === 'p') {
-//         e.preventDefault();
-//     }
-//     if (e.ctrlKey && (e.key === 'p' || e.key === 's')) {
-//             e.preventDefault();
-//     }
-//   };
+const handleMenu = () => {
+  toggleMenu.value = !toggleMenu.value;
+}
 
-//   window.print = function() {
-//     alert('Printing is disabled on this page.');
-//   };
-// })
 </script>
 
 <template>
   <button
+    @click="handleMenu"
     data-drawer-target="default-sidebar"
     data-drawer-toggle="default-sidebar"
     aria-controls="default-sidebar"
     type="button"
-    class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+    :class="toggleMenu ? 'hidden' : 'inline-flex'"
+    class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
   >
     <span class="sr-only">Open sidebar</span>
     <svg
@@ -67,10 +42,14 @@ const isShowDropDown2 = ref(false);
 
   <aside
     id="default-sidebar"
+    :class="toggleMenu ? 'translate-x-0' : '-translate-x-full'"
     class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full md:translate-x-0"
     aria-label="Sidebar"
   >
     <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50">
+      <div class="mb-2 text-end block md:hidden">
+        <span @click="handleMenu" class="text-[30px] text-gray-700 px-2 cursor-pointer rounded hover:bg-gray-200">&times;</span>
+      </div>
       <img class="h-15 p-2 mb-5" src="/images/logo-without-bg.png" alt="">
       <ul class="space-y-2 font-medium">
         <li>
@@ -139,58 +118,6 @@ const isShowDropDown2 = ref(false);
           <span class="flex-1 ms-3 whitespace-nowrap">Post</span>
           </Link>
         </li>
-        <!-- <li>
-          <button
-            @click="isShowDropDown = !isShowDropDown"
-            type="button"
-            class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100"
-            aria-controls="dropdown-example"
-            data-collapse-toggle="dropdown-example"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"
-              fill="currentColor"
-              viewBox="0 0 18 16"
-            >
-              <path d="M4.98 1a.5.5 0 0 0-.39.188L1.54 5H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0A.5.5 0 0 1 10 5h4.46l-3.05-3.812A.5.5 0 0 0 11.02 1zm9.954 5H10.45a2.5 2.5 0 0 1-4.9 0H1.066l.32 2.562A.5.5 0 0 0 1.884 9h12.234a.5.5 0 0 0 .496-.438zM3.809.563A1.5 1.5 0 0 1 4.981 0h6.038a1.5 1.5 0 0 1 1.172.563l3.7 4.625a.5.5 0 0 1 .105.374l-.39 3.124A1.5 1.5 0 0 1 14.117 10H1.883A1.5 1.5 0 0 1 .394 8.686l-.39-3.124a.5.5 0 0 1 .106-.374zM.125 11.17A.5.5 0 0 1 .5 11H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0 .5.5 0 0 1 .5-.5h5.5a.5.5 0 0 1 .496.562l-.39 3.124A1.5 1.5 0 0 1 14.117 16H1.883a1.5 1.5 0 0 1-1.489-1.314l-.39-3.124a.5.5 0 0 1 .121-.393zm.941.83.32 2.562a.5.5 0 0 0 .497.438h12.234a.5.5 0 0 0 .496-.438l.32-2.562H10.45a2.5 2.5 0 0 1-4.9 0z" />
-            </svg>
-            <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Post</span>
-            <svg
-              class="w-3 h-3"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 10 6"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m1 1 4 4 4-4"
-              />
-            </svg>
-          </button>
-          <ul
-            v-show="isShowDropDown"
-            id="dropdown-example"
-            class="py-2 space-y-2"
-          >
-            <li>
-              <Link
-                :href="route('admin.achievement.index')"
-                class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100"
-              >Achievements</Link>
-            </li>
-            <li>
-              <Link
-                :href="route('admin.announcement.index')"
-                class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100"
-              >Announcements</Link>
-            </li>
-          </ul>
-        </li> -->
         <li>
           <button
             @click="isShowDropDown2 = !isShowDropDown2"
