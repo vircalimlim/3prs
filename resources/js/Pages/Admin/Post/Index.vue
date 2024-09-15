8d8ed3f760c0f45dcae4e6a79c6a74ed6acae6d7
vircalimlim <killerfinisher12@gmail.com>
2024-09-15 16:12:38 +0800

    admin > add empty message.

--- resources/js/Pages/Admin/Post/Index.vue
+++ resources/js/Pages/Admin/Post/Index.vue
@@ -8,18 +8,18 @@ import { reactive, ref } from "vue";
 import Pagination from "@/Components/Pagination.vue";
 import { onMounted } from "vue";
 
-const {posts} = defineProps({
-    posts: {
-      type: Object,
-      required: true,
-    },
-    storage_link: {
-      type: String,
-      required: true,
-    },
+const { posts } = defineProps({
+  posts: {
+    type: Object,
+    required: true,
+  },
+  storage_link: {
+    type: String,
+    required: true,
+  },
 });
 
-const filteredPost = ref('achievements');
+const filteredPost = ref("achievements");
 const showAdd = ref(false);
 const showEdit = ref(false);
 const showImage = ref(false);
@@ -39,95 +39,166 @@ const openEditModal = (post: DynamicObject) => {
 };
 
 const openImageModal = (post: DynamicObject) => {
-    showImage.value = true;
-    post.type = filteredPost.value;
-    Object.assign(editPost, post);
-}
+  showImage.value = true;
+  post.type = filteredPost.value;
+  Object.assign(editPost, post);
+};
 
 const getFilter = () => {
-  const defaultValue = 'achievements';
+  const defaultValue = "achievements";
   const url = new URL(window.location.href);
   const params = new URLSearchParams(url.search);
-  const typeValue = params.get('type') || defaultValue;
+  const typeValue = params.get("type") || defaultValue;
   filteredPost.value = typeValue;
-}
+};
 
 const handleTypeFilter = () => {
-    router.get('/admin/post', {type: filteredPost.value}, {preserveState: true, preserveScroll: true});
-}
+  router.get(
+    "/admin/post",
+    { type: filteredPost.value },
+    { preserveState: true, preserveScroll: true }
+  );
+};
 
 onMounted(() => {
   getFilter();
-})
+});
 </script>
 
 <template>
+
   <Head title="Dashboard" />
 
   <Admin>
     <section>
-    <div class="flex justify-between items-center">
-      <h1
-        class="my-5 text-4xl font-extrabold tracking-tight leading-none text-gray-600 capitalize"
-      >
-        {{filteredPost}}
-      </h1>
-      <div class="flex items-center gap-2 mb-4">
-        <select @change="handleTypeFilter" v-model="filteredPost" id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 py-2">
-            <option value="" disabled selected>Filter by</option>
+      <div class="flex justify-between items-center">
+        <h1 class="my-5 text-4xl font-extrabold tracking-tight leading-none text-gray-600 capitalize">
+          {{filteredPost}}
+        </h1>
+        <div class="flex items-center gap-2 mb-4">
+          <select
+            @change="handleTypeFilter"
+            v-model="filteredPost"
+            id="countries"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 py-2"
+          >
+            <option
+              value=""
+              disabled
+              selected
+            >Filter by</option>
             <option value="achievements">Achievements</option>
             <option value="announcements">Announcements</option>
-        </select>
+          </select>
 
-        <button @click.prevent="openAddModal" type="button" class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center">
+          <button
+            @click.prevent="openAddModal"
+            type="button"
+            class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center"
+          >
             Add
-        </button>
+          </button>
+        </div>
       </div>
-    </div>
 
       <div class="relative overflow-x-auto shadow sm:rounded-lg">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500">
           <thead class="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
-              <th scope="col" class="px-6 py-3">Image</th>
-              <th scope="col" class="px-6 py-3">Title</th>
-              <th scope="col" class="px-6 py-3">Description</th>
-              <th scope="col" class="px-6 py-3">
+              <th
+                scope="col"
+                class="px-6 py-3"
+              >Image</th>
+              <th
+                scope="col"
+                class="px-6 py-3"
+              >Title</th>
+              <th
+                scope="col"
+                class="px-6 py-3"
+              >Description</th>
+              <th
+                scope="col"
+                class="px-6 py-3"
+              >
                 <span class="sr-only">Edit</span>
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="post in posts.data" class="bg-white border-b">
-                <td class="w-96 px-2 py-4 whitespace-nowrap align-top">
-                    <div @click.prevent="openImageModal(post)" class="relative group">
-                        <div class="absolute inset-0 flex items-center justify-center rounded bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 cursor-pointer">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-camera text-white" viewBox="0 0 16 16">
-                                <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4z"/>
-                                <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5m0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7M3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0"/>
-                            </svg>
-                        </div>
+            <tr
+              v-for="post in posts.data"
+              class="bg-white border-b"
+            >
+              <td class="w-96 px-2 py-4 whitespace-nowrap align-top">
+                <div
+                  @click.prevent="openImageModal(post)"
+                  class="relative group"
+                >
+                  <div class="absolute inset-0 flex items-center justify-center rounded bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 cursor-pointer">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="40"
+                      height="40"
+                      fill="currentColor"
+                      class="bi bi-camera text-white"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4z" />
+                      <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5m0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7M3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0" />
+                    </svg>
+                  </div>
 
-                        <img class="h-auto w-auto md:h-[350px] md:w-full object-cover rounded" :src="storage_link + '/' + post.image" alt="image description">
-                    </div>
-                </td>
+                  <img
+                    class="h-auto w-auto md:h-[350px] md:w-full object-cover rounded"
+                    :src="storage_link + '/' + post.image"
+                    alt="image description"
+                  >
+                </div>
+              </td>
 
               <td class="font-bold px-4 py-4 align-top">
                 {{post.title}}
               </td>
-              <td class="px-6 py-4 inline-block" v-html="post.description">
+              <td
+                class="px-6 py-4 inline-block"
+                v-html="post.description"
+              >
               </td>
               <td class="px-6 py-4 text-right align-top">
-                <a @click.prevent="openEditModal(post)" href="#" class="font-medium text-blue-600 hover:underline">Edit</a>
+                <a
+                  @click.prevent="openEditModal(post)"
+                  href="#"
+                  class="font-medium text-blue-600 hover:underline"
+                >Edit</a>
               </td>
+            </tr>
+            <tr v-if="posts.data.length == 0">
+              <td
+                class="text-center"
+                colspan="4"
+              >No records found.</td>
             </tr>
           </tbody>
         </table>
       </div>
-      <Pagination :links="posts.links"/>
+      <Pagination :links="posts.links" />
     </section>
-    <AddModal :showAdd="showAdd" @close="showAdd = false" />
-    <EditModal v-if="showEdit" :showEdit="showEdit" @close="showEdit = false" :post="editPost" />
-    <ImageModal v-if="showImage" :showImage="showImage" @close="showImage = false" :post="editPost" />
+    <AddModal
+      :showAdd="showAdd"
+      @close="showAdd = false"
+    />
+    <EditModal
+      v-if="showEdit"
+      :showEdit="showEdit"
+      @close="showEdit = false"
+      :post="editPost"
+    />
+    <ImageModal
+      v-if="showImage"
+      :showImage="showImage"
+      @close="showImage = false"
+      :post="editPost"
+    />
   </Admin>
 </template>
--- resources/js/Pages/Admin/Student/Index.vue
+++ resources/js/Pages/Admin/Student/Index.vue
@@ -6,7 +6,7 @@ import EditModal from "./Partials/EditModal.vue";
 import { ref, reactive } from "vue";
 import Pagination from "@/Components/Pagination.vue";
 
-const {students} = defineProps({
+const { students } = defineProps({
   students: {
     type: Object,
     required: true,
@@ -32,39 +32,66 @@ const openEditModal = (student: DynamicObject) => {
 </script>
 
 <template>
+
   <Head title="Dashboard" />
 
   <Admin>
     <section>
-    <div class="flex justify-between items-center">
-      <h1
-        class="my-5 text-4xl font-extrabold tracking-tight leading-none text-gray-600"
-      >
-        Student
-      </h1>
-      <button @click.prevent="openAddModal" type="button" class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center me-2 mb-2">
-        Add
-      </button>
-    </div>
+      <div class="flex justify-between items-center">
+        <h1 class="my-5 text-4xl font-extrabold tracking-tight leading-none text-gray-600">
+          Student
+        </h1>
+        <button
+          @click.prevent="openAddModal"
+          type="button"
+          class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center me-2 mb-2"
+        >
+          Add
+        </button>
+      </div>
 
       <div class="relative overflow-x-auto shadow sm:rounded-lg">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500">
           <thead class="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
               <!-- <th scope="col" class="px-6 py-3">STUDENT ID</th> -->
-              <th scope="col" class="px-6 py-3">Full Name</th>
-              <th scope="col" class="px-6 py-3">Gender</th>
-              <th scope="col" class="px-6 py-3">DOB</th>
-              <th scope="col" class="px-6 py-3">EMAIL</th>
-              <th scope="col" class="px-6 py-3">MOBILE</th>
-              <th scope="col" class="px-6 py-3">ADDRESS</th>
-              <th scope="col" class="px-6 py-3">
+              <th
+                scope="col"
+                class="px-6 py-3"
+              >Full Name</th>
+              <th
+                scope="col"
+                class="px-6 py-3"
+              >Gender</th>
+              <th
+                scope="col"
+                class="px-6 py-3"
+              >DOB</th>
+              <th
+                scope="col"
+                class="px-6 py-3"
+              >EMAIL</th>
+              <th
+                scope="col"
+                class="px-6 py-3"
+              >MOBILE</th>
+              <th
+                scope="col"
+                class="px-6 py-3"
+              >ADDRESS</th>
+              <th
+                scope="col"
+                class="px-6 py-3"
+              >
                 <span class="sr-only">Edit</span>
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="student in students.data" class="bg-white border-b">
+            <tr
+              v-for="student in students.data"
+              class="bg-white border-b"
+            >
               <!-- <th
                 scope="row"
                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
@@ -78,15 +105,33 @@ const openEditModal = (student: DynamicObject) => {
               <td class="px-6 py-4">{{student.mobile}}</td>
               <td class="px-6 py-4 capitalize">{{student.address}}</td>
               <td class="px-6 py-4 text-right">
-                <a @click.prevent="openEditModal(student)" href="#" class="font-medium text-blue-600 hover:underline">Edit</a>
+                <a
+                  @click.prevent="openEditModal(student)"
+                  href="#"
+                  class="font-medium text-blue-600 hover:underline"
+                >Edit</a>
               </td>
+            </tr>
+            <tr v-if="students.data.length == 0">
+              <td
+                class="text-center"
+                colspan="7"
+              >No records found.</td>
             </tr>
           </tbody>
         </table>
-        <Pagination :links="students.links"/>
+        <Pagination :links="students.links" />
       </div>
     </section>
-    <AddModal :showAdd="showAdd" @close="showAdd = false" />
-    <EditModal v-if="showEdit" :showEdit="showEdit" @close="showEdit = false" :student="editStudent" />
+    <AddModal
+      :showAdd="showAdd"
+      @close="showAdd = false"
+    />
+    <EditModal
+      v-if="showEdit"
+      :showEdit="showEdit"
+      @close="showEdit = false"
+      :student="editStudent"
+    />
   </Admin>
 </template>
