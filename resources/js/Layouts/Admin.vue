9d27a5b0c5d856ddfbbe2a8fdbbf1d381204f79c
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-09-18 17:46:23 +0800

    admin > update sidebar icons.

--- resources/js/Layouts/Admin.vue
+++ resources/js/Layouts/Admin.vue
@@ -63,16 +63,7 @@ const handleMenu = () => {
             :href="route('dashboard')"
             class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
           >
-          <svg
-            class="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 22 21"
-          >
-            <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-            <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-          </svg>
+          <img class="h-8 w-8" src="/images/icons/dashboard.png" />
           <span class="ms-3">Dashboard</span>
           </Link>
         </li>
@@ -81,14 +72,7 @@ const handleMenu = () => {
             :href="route('semester.index')"
             class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
           >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"
-            fill="currentColor"
-            viewBox="0 0 18 16"
-          >
-            <path d="M0 5a5 5 0 0 0 4.027 4.905 6.5 6.5 0 0 1 .544-2.073C3.695 7.536 3.132 6.864 3 5.91h-.5v-.426h.466V5.05q-.001-.07.004-.135H2.5v-.427h.511C3.236 3.24 4.213 2.5 5.681 2.5c.316 0 .59.031.819.085v.733a3.5 3.5 0 0 0-.815-.082c-.919 0-1.538.466-1.734 1.252h1.917v.427h-1.98q-.004.07-.003.147v.422h1.983v.427H3.93c.118.602.468 1.03 1.005 1.229a6.5 6.5 0 0 1 4.97-3.113A5.002 5.002 0 0 0 0 5m16 5.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0m-7.75 1.322c.069.835.746 1.485 1.964 1.562V14h.54v-.62c1.259-.086 1.996-.74 1.996-1.69 0-.865-.563-1.31-1.57-1.54l-.426-.1V8.374c.54.06.884.347.966.745h.948c-.07-.804-.779-1.433-1.914-1.502V7h-.54v.629c-1.076.103-1.808.732-1.808 1.622 0 .787.544 1.288 1.45 1.493l.358.085v1.78c-.554-.08-.92-.376-1.003-.787zm1.96-1.895c-.532-.12-.82-.364-.82-.732 0-.41.311-.719.824-.809v1.54h-.005zm.622 1.044c.645.145.943.38.943.796 0 .474-.37.8-1.02.86v-1.674z" />
-          </svg>
+          <img class="h-8 w-8" src="/images/icons/semester.png" />
           <span class="flex-1 ms-3 whitespace-nowrap">Semester</span>
           </Link>
         </li>
@@ -97,14 +81,7 @@ const handleMenu = () => {
             :href="route('admin.student.index')"
             class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
           >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"
-            fill="currentColor"
-            viewBox="0 0 18 16"
-          >
-            <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
-          </svg>
+          <img class="h-8 w-8" src="/images/icons/student.png" />
           <span class="flex-1 ms-3 whitespace-nowrap">Student</span>
           </Link>
         </li>
@@ -113,14 +90,7 @@ const handleMenu = () => {
             :href="route('admin.post.index')"
             class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
           >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"
-            fill="currentColor"
-            viewBox="0 0 18 16"
-          >
-            <path d="M4.98 1a.5.5 0 0 0-.39.188L1.54 5H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0A.5.5 0 0 1 10 5h4.46l-3.05-3.812A.5.5 0 0 0 11.02 1zm9.954 5H10.45a2.5 2.5 0 0 1-4.9 0H1.066l.32 2.562A.5.5 0 0 0 1.884 9h12.234a.5.5 0 0 0 .496-.438zM3.809.563A1.5 1.5 0 0 1 4.981 0h6.038a1.5 1.5 0 0 1 1.172.563l3.7 4.625a.5.5 0 0 1 .105.374l-.39 3.124A1.5 1.5 0 0 1 14.117 10H1.883A1.5 1.5 0 0 1 .394 8.686l-.39-3.124a.5.5 0 0 1 .106-.374zM.125 11.17A.5.5 0 0 1 .5 11H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0 .5.5 0 0 1 .5-.5h5.5a.5.5 0 0 1 .496.562l-.39 3.124A1.5 1.5 0 0 1 14.117 16H1.883a1.5 1.5 0 0 1-1.489-1.314l-.39-3.124a.5.5 0 0 1 .121-.393zm.941.83.32 2.562a.5.5 0 0 0 .497.438h12.234a.5.5 0 0 0 .496-.438l.32-2.562H10.45a2.5 2.5 0 0 1-4.9 0z" />
-          </svg>
+          <img class="h-8 w-8" src="/images/icons/post.png" />
           <span class="flex-1 ms-3 whitespace-nowrap">Post</span>
           </Link>
         </li>
@@ -132,14 +102,7 @@ const handleMenu = () => {
             aria-controls="dropdown-example"
             data-collapse-toggle="dropdown-example"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"
-              fill="currentColor"
-              viewBox="0 0 18 16"
-            >
-              <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
-            </svg>
+            <img class="h-8 w-8" src="/images/icons/research.png" />
             <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Research</span>
             <svg
               class="w-3 h-3"
@@ -181,14 +144,7 @@ const handleMenu = () => {
             :href="route('admin.logs')"
             class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
           >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"
-            fill="currentColor"
-            viewBox="0 0 18 16"
-          >
-            <path d="M4.98 1a.5.5 0 0 0-.39.188L1.54 5H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0A.5.5 0 0 1 10 5h4.46l-3.05-3.812A.5.5 0 0 0 11.02 1zm9.954 5H10.45a2.5 2.5 0 0 1-4.9 0H1.066l.32 2.562A.5.5 0 0 0 1.884 9h12.234a.5.5 0 0 0 .496-.438zM3.809.563A1.5 1.5 0 0 1 4.981 0h6.038a1.5 1.5 0 0 1 1.172.563l3.7 4.625a.5.5 0 0 1 .105.374l-.39 3.124A1.5 1.5 0 0 1 14.117 10H1.883A1.5 1.5 0 0 1 .394 8.686l-.39-3.124a.5.5 0 0 1 .106-.374zM.125 11.17A.5.5 0 0 1 .5 11H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0 .5.5 0 0 1 .5-.5h5.5a.5.5 0 0 1 .496.562l-.39 3.124A1.5 1.5 0 0 1 14.117 16H1.883a1.5 1.5 0 0 1-1.489-1.314l-.39-3.124a.5.5 0 0 1 .121-.393zm.941.83.32 2.562a.5.5 0 0 0 .497.438h12.234a.5.5 0 0 0 .496-.438l.32-2.562H10.45a2.5 2.5 0 0 1-4.9 0z" />
-          </svg>
+          <img class="h-8 w-8" src="/images/icons/logs.png" />
           <span class="flex-1 ms-3 whitespace-nowrap">Logs</span>
           </Link>
         </li>
@@ -199,21 +155,7 @@ const handleMenu = () => {
             as="button"
             class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
           >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"
-            fill="currentColor"
-            viewBox="0 0 18 16"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"
-            />
-            <path
-              fill-rule="evenodd"
-              d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
-            />
-          </svg>
+          <img class="h-8 w-8" src="/images/icons/logout.png" />
           <span class="flex-1 ms-3 whitespace-nowrap">Logout</span>
           </Link>
         </li>
