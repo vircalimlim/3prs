e238b85ce04589fbb7a736509dd072d2a156c93b
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-09-18 16:56:34 +0800

    admin > chart in dashboard.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,11 +9,29 @@ use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
+
+    private function getLogsPerMonth(){
+        $year = Carbon::now()->year;
+        $tally = [];
+        for ($i = 1; $i <= 12; $i++) {
+            $month = $i;
+            $month_word = Carbon::create()->month($month)->format('M');
+            $total = DB::table('user_visit')->whereYear('created_at', $year)
+            ->whereMonth('created_at', $month)
+            ->count();
+
+            $tally[] = (object) ['month' => $month_word,'total' => $total];
+        }
+        return $tally;
+    }
+
     public function index()
     {
+        $research_visit = $this->getLogsPerMonth();
         $today = date('Y-m-d');
         $total_students         = Student::count();
         $total_enrolled         = DB::table('semester_student as st')
@@ -31,6 +49,7 @@ class DashboardController extends Controller
             'total_achievements'    => $total_achievements,
             'total_announcements'   => $total_announcements,
             'total_materials'       => $total_materials,
+            'research_visit'        => $research_visit,
         ]);
     }
 
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 import Admin from '@/Layouts/Admin.vue';
 import { Head, Link } from '@inertiajs/vue3';
-const props = defineProps({
+
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { ref, onMounted, computed } from 'vue';
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+const {research_visit} = defineProps({
     'total_students': {
         type: Number,
         required: true
@@ -21,14 +27,46 @@ const props = defineProps({
     'total_materials': {
         type: Number,
         required: true
+    },
+    'research_visit': {
+        type: Array<any>,
+        required: true,
     }
 });
+
+const monthLabel = ref();
+const monthTotal = ref();
+
+const chartData = computed(() => ({
+  labels: monthLabel.value,
+  datasets: [
+    {
+      label: 'Research Visit',
+      backgroundColor: '#000080',
+      data: monthTotal.value,
+    },
+  ],
+}));
+
+const chartOptions = ref({
+  responsive: true,
+  maintainAspectRatio: false
+})
+
+onMounted(() => {
+    monthLabel.value = research_visit.map(research => research.month);
+    monthTotal.value = research_visit.map(research => research.total);
+    console.log(monthLabel.value)
+})
 </script>
 
 <template>
     <Head title="Dashboard" />
 
     <Admin>
+        <div class="h-72">
+            <Bar :data="chartData" :options="chartOptions" />
+        </div>
         <!-- <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -36,30 +74,30 @@ const props = defineProps({
                 </div>
             </div>
         </div> -->
-        <div class="md:h-full flex items-center justify-center">
-            <div class="flex flex-wrap gap-10 justify-center">
+        <div class="">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center sm:justify-items-start gap-5 py-4">
             
-                <Link :href="route('admin.student.index')" class="flex flex-col gap-2 items-center justify-center max-w-sm py-6 px-20 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                <Link :href="route('admin.student.index')" class="flex flex-col gap-2 items-center justify-center w-full max-w-sm py-6 px-20 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
                     <h3 class="text-xl font-medium text-blue-900">Total Students</h3>
                     <h1 class="font-bold text-6xl text-gray-700">{{total_students}}</h1>
                 </Link>
 
-                <Link :href="route('semester.enrolled_student')" class="flex flex-col gap-2 items-center justify-center max-w-sm py-6 px-20 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                <Link :href="route('semester.enrolled_student')" class="flex flex-col gap-2 items-center justify-center w-full max-w-sm py-6 px-20 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
                     <h3 class="text-xl font-medium text-blue-900">Total Enrolled</h3>
                     <h1 class="font-bold text-6xl text-gray-700">{{ total_enrolled }}</h1>
                 </Link>
 
-                <Link :href="route('admin.post.index')" class="flex flex-col gap-2 items-center justify-center max-w-sm py-6 px-20 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                <Link :href="route('admin.post.index')" class="flex flex-col gap-2 items-center justify-center w-full max-w-sm py-6 px-20 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
                     <h3 class="text-xl font-medium text-blue-900">Achievements</h3>
                     <h1 class="font-bold text-6xl text-gray-700">{{total_achievements}}</h1>
                 </Link>
 
-                <Link :href="route('admin.post.index')" class="flex flex-col gap-2 items-center justify-center max-w-sm py-6 px-20 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                <Link :href="route('admin.post.index')" class="flex flex-col gap-2 items-center justify-center w-full max-w-sm py-6 px-20 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
                     <h3 class="text-xl font-medium text-blue-900">Announcements</h3>
                     <h1 class="font-bold text-6xl text-gray-700">{{ total_announcements }}</h1>
                 </Link>
 
-                <Link :href="route('admin.material.index')" class="flex flex-col gap-2 items-center justify-center max-w-sm py-6 px-20 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                <Link :href="route('admin.material.index')" class="flex flex-col gap-2 items-center justify-center w-full max-w-sm py-6 px-20 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
                     <h3 class="text-xl font-medium text-blue-900">Research Materials</h3>
                     <h1 class="font-bold text-6xl text-gray-700">{{ total_materials }}</h1>
                 </Link>
