9e0895fe80eab254a51ec44826283bdd0289764e
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-09-24 17:54:44 +0800

    Update Welcome.vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -78,7 +78,7 @@ header {
     >
       <div class="max-w-screen-xl flex flex-wraps items-center justify-between mx-auto p-4">
         <a
-          href="https://flowbite.com/"
+          href="/"
           class="flex items-center space-x-3 rtl:space-x-reverse"
         >
           <img
