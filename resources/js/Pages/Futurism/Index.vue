b328ba232f56a6b263c86ca2e8bac9b35eff5c9b
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-10-18 17:52:56 +0800

    public > apply richtext truncate using dotdotdot in futurism.

--- resources/js/Pages/Futurism/Index.vue
+++ resources/js/Pages/Futurism/Index.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import { ref, onMounted, onUnmounted } from "vue";
 import StudentLayout from '@/Layouts/StudentLayout.vue';
 import { Link } from '@inertiajs/vue3';
+import Dotdotdot from 'dotdotdot-js';
 
 const {category} = defineProps({
     'futurisms': {
@@ -17,6 +19,8 @@ const {category} = defineProps({
     }
 });
 
+const clampText = ref([]);
+
 const formatDate = (inputDate: string) => {
     const date = new Date(inputDate.split(' ')[0]);
 
@@ -26,6 +30,28 @@ const formatDate = (inputDate: string) => {
 
     return `${month}/${day}/${year}`;
 }
+
+onMounted(() => {
+
+if (clampText.value.length > 1) {
+  // Apply Dotdotdot to each element in the array
+  clampText.value.forEach(text => {
+    if (text) {
+      new Dotdotdot(text, {
+        height: 100,  // Set max height for the text container
+        truncate: 'letter'  // Truncate by letter, can be 'word' or 'node'
+      });
+    }
+  });
+} else if (clampText.value.length === 1 && clampText.value[0]) {
+  // Handle single element case
+  new Dotdotdot(clampText.value[0], {
+    height: 100,
+    truncate: 'letter'
+  });
+}
+
+});
 </script>
 
 <template>
@@ -37,7 +63,7 @@ const formatDate = (inputDate: string) => {
                 <div class="h-full flex flex-col justify-between p-4 leading-normal">
                     <h5 class="text-2xl font-bold tracking-tight text-gray-900 capitalize">{{futurism.title}}</h5>
                     <p class="text-[14px] font-bold text-gray-600 mb-2">{{ formatDate(futurism.created_at) }}</p>
-                    <p v-html="futurism.description" class="mb-3 font-normal text-gray-700 line-clamp-5"></p>
+                    <div ref="clampText" v-html="futurism.description" class="mb-3 font-normal text-gray-700 line-clamp-5"></div>
                     <div class="mt-5">
                         <Link :href="`/futurism/${futurism.id}`"
                             class="py-2 px-3 text-base font-medium text-center text-blue-700 rounded-lg border border-border-800 shadow-md hover:border-blue-800 hover:text-white hover:text-grey-300 hover:bg-blue-800">
@@ -53,3 +79,16 @@ const formatDate = (inputDate: string) => {
         </section>
     </StudentLayout>
 </template>
+
+<style scoped>
+    .line-clamp-5 {
+    display: -webkit-box;
+    -webkit-line-clamp: 5;
+    line-clamp: 5;
+    -webkit-box-pack: end;
+    text-overflow: ellipsis;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-align: left;
+    }
+</style>
