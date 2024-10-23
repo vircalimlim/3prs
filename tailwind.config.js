35abe43d24278096eef48e130f658bbeb0687391
vircalimlim <62140648+vircalimlim@users.noreply.github.com>
2024-10-23 11:14:50 +0800

    add Akko Pro Regular font.

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Akko Pro Regular', ...defaultTheme.fontFamily.sans],
             },
         },
     },
